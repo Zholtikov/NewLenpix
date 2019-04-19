6a5379211a8655eb1622374f612e5e1c8822133f
Roman Sosnovsky <rvsosnovsky@gmail.com>
2019-04-19 15:40:56 +0300

    Добавил пустой gradle-проект

---
+++ src/main/java/ru/lenpix/Main.java
@@ -0,0 +1,11 @@
+package ru.lenpix;
+
+/**
+ * Пример точки входа.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/ru/lenpix/MainTest.java
@@ -0,0 +1,19 @@
+package ru.lenpix;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Пример теста.
+ */
+class MainTest {
+
+    @Test
+    void test_first() {
+        System.out.println("First simple test");
+    }
+
+    @Test
+    void test_second() {
+        System.out.println("Second simple test");
+    }
+}
