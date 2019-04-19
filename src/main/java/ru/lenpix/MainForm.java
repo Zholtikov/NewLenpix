6e6fe66ef18fe552cd21e3860262e1fc3411457b
goltikov <vladislav13590@mail.ru>
2019-04-19 18:27:55 +0300

    Реализовал форму с отображением пользовательских изображений

--- src/main/java/ru/lenpix/MainForm.java
+++ src/main/java/ru/lenpix/MainForm.java
@@ -1,9 +1,24 @@
 package ru.lenpix;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.MenuBar;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 
 /**
  * Пример javafx-окошечка.
@@ -14,13 +29,58 @@ public class MainForm extends Application {
         launch(args);
     }
 
+    private Image leftImage, rightImage;
+    private boolean showLeft = true;
+    private Stage primaryStage;
+
+    @FXML
+    private Canvas canvas;
+
     @Override
     public void start(Stage primaryStage) {
-        FlowPane root = new FlowPane();
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setWidth(300);
-        primaryStage.setHeight(250);
-        primaryStage.show();
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("main.fxml"));
+
+            Scene scene = new Scene(parent);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleSubmitButtonAction(ActionEvent event) {
+        List<File> files = new FileChooser().showOpenMultipleDialog(primaryStage);
+        if (files != null && files.size() == 2) {
+            leftImage = new Image(files.get(0).toURI().toString());
+            rightImage = new Image(files.get(1).toURI().toString());
+            repaintCanvas();
+        }
+    }
+
+    @FXML
+    public void changeImageOnCanvas(ActionEvent event) {
+        showLeft = !showLeft;
+        repaintCanvas();
+    }
+
+    private void repaintCanvas() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        if (leftImage  == null || rightImage == null) {
+            gc.setFill(Color.BLACK);
+            gc.fill();
+        }
+
+        drawImageOnCanvas((showLeft ? leftImage : rightImage));
+    }
+
+    private void drawImageOnCanvas(Image image) {
+        canvas.setWidth(image.getWidth());
+        canvas.setHeight(image.getHeight());
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight());
     }
 }
