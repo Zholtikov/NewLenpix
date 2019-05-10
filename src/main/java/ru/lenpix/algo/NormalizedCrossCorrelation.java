d9251e10e4e8af474f33099c14f3d025f58287ef
goltikov <vladislav13590@mail.ru>
2019-05-10 17:16:39 +0300

    Оптимизация алгоритма

--- src/main/java/ru/lenpix/algo/NormalizedCrossCorrelation.java
+++ src/main/java/ru/lenpix/algo/NormalizedCrossCorrelation.java
@@ -10,6 +10,9 @@ public class NormalizedCrossCorrelation {
     private final int n;
     private final int x;
     private final int y;
+    private double firstIsumm;
+    private double firstISqSumm;
+    private boolean inited = false;
 
     public NormalizedCrossCorrelation(DoubleMatrix mat1, DoubleMatrix mat2, int n, int x, int y) {
         this.mat1 = mat1;
@@ -19,41 +22,62 @@ public class NormalizedCrossCorrelation {
         this.y = y;
     }
 
+    // считаем сумму I1 с квадратами (она не меняется)
+    private double countSqSumFirst(DoubleMatrix mat1, int n, int x, int y, double firstSum) {
+        double resSq = 0.0;
+        for (int i = x; i < x + n; i++) {
+            for (int j = y; j < y + n; j++) {
+                resSq += Math.pow(mat1.get(i, j), 2) - Math.pow(firstSum, 2) / (n * n);
+            }
+        }
+        return resSq;
+    }
+
+    // считаем сумму I1 (она не меняется)
+    private double countSumOfFirst(DoubleMatrix mat1, int n, int x, int y) {
+        double res = 0.0;
+        for (int i = x; i < x + n; i++) {
+            for (int j = y; j < y + n; j++) {
+                res += mat1.get(i, j);
+            }
+        }
+        return res;
+    }
+
     public double apply(int dx, int dy) {
-        double nominator = sum(x, x + n, y, y + n,
-                (i, j) -> func(mat1, i, j, 0, 0) * func(mat2, i, j, dx, dy));
+        if (!inited) {
+            firstIsumm = countSumOfFirst(mat1, n, x, y);
+            firstISqSumm = countSqSumFirst(mat1, n, x, y, firstIsumm);
+            inited = true;
+        }
 
-        double a = sum(x, x + n, y, y + n,
-                (i, j) -> Math.pow(func(mat1, i, j, 0, 0), 2));
-
-        double b = sum(x, x + n, y, y + n,
-                (i, j) -> Math.pow(func(mat2, i, j, dx, dy), 2));
-
-
-        double denominator = sqrt(a * b);
-
-        return nominator / denominator;
-    }
-
-    private double func(DoubleMatrix mat, int x, int y, int dx, int dy) {
-        return mat.get(x + dx, y + dy) - normI(mat, x + dx, y + dy);
-    }
-
-    private double normI(DoubleMatrix mat, int x, int y) {
-        double sum = sum(x, x + n, y, y + n, mat::get);
-
-        return sum / (n * n);
-    }
-
-    private double sum(int minI, int maxI, int minJ, int maxJ, BiFunction<Integer, Integer, Double> func) {
-        double ret = 0;
-
-        for (int i = minI; i < maxI; i++) {
-            for (int j = minJ; j < maxJ; j++) {
-                ret += func.apply(i, j);
+        // считаем сумму I2
+        double secondIsum = 0.0;
+        for (int i = x; i < x + n; i++) {
+            for (int j = y; j < y + n; j++) {
+                secondIsum += mat2.get(i + dx, j + dx);
             }
         }
 
-        return ret;
+        // считаем сумму I2 с квадратами
+        double secondISqSum = 0.0;
+        for (int i = x; i < x + n; i++) {
+            for (int j = y; j < y + n; j++) {
+                secondISqSum += Math.pow(mat2.get(i + dx, j + dy), 2) - Math.pow(secondIsum, 2) / (n * n);
+            }
+        }
+
+        // считаем знаменатель
+        double det = Math.sqrt(firstISqSumm * secondISqSum);
+
+        // считаем числитель
+        double num = 0.0;
+        for (int i = x; i < x + n; i++) {
+            for (int j = y; j < y + n; j++) {
+                num += mat1.get(i, j) * mat2.get(i + dx, j + dy) - firstIsumm * secondIsum / (n * n);
+            }
+        }
+
+        return num / det;
     }
 }
